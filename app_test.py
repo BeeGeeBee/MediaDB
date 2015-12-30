1a1d293ac5dacdbb87cc6bf396c0b5ee16b87c89
bernie <bernie@home.com>
2015-12-30 02:08:43 +0000

    Initial tests for books and dvd.

--- app_test.py
+++ app_test.py
@@ -24,7 +24,25 @@ class ComponentsTestCase(unittest.TestCase):
         mediatest = MediaObject(category='Books')
         response = mediatest.getbybarcode()
         assert '<ERROR> No barcode defined.' in response
+        mediatest.barcode = '9780007304461'
+        response = mediatest.getbybarcode()
+        assert '<WARNING>Barcode 9780007304461 not found on www.yoopsie.com in Books' in response
         mediatest.barcode = '9780007304462'
         response = mediatest.getbybarcode()
-        print response
         assert '<OK>Azincourt:' in response
+
+
+    def test_02_dvds(self):
+        mediatest = MediaObject()
+        mediatest.barcode = '5030305514860'
+        response = mediatest.getbybarcode()
+        assert '<ERROR> No category defined.' in response
+        mediatest = MediaObject(category='DVD')
+        response = mediatest.getbybarcode()
+        assert '<ERROR> No barcode defined.' in response
+        mediatest.barcode = '5030305514861'
+        response = mediatest.getbybarcode()
+        assert '<WARNING>Barcode 5030305514861 not found on www.yoopsie.com in DVD' in response
+        mediatest.barcode = '5030305514860'
+        response = mediatest.getbybarcode()
+        assert '<OK>Gnomeo & Juliet [DVD]:' in response
